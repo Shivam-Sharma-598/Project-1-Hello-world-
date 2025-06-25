50ad50525cb7bb472e52ba3009fbf60065fd9842
Shivam Sharma <sharma.tech.59@gmail.com>
2025-06-25 17:05:44 +0530

    Light/Dark mode completed

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const buttonjs = document.getElementById('button')
 
-buttonjs.addEventListener('click' , () => {
+buttonjs.addEventListener('click', () => {
   document.body.classList.toggle('darkMode');
-})
+
+  if (document.body.classList.contains('darkMode')) {
+    buttonjs.textContent = 'Light Mode';
+  } else {
+    buttonjs.textContent = 'Dark Mode';
+  }
+
+});
